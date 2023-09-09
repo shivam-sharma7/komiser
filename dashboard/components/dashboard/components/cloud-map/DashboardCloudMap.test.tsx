15b3936563ae2cd306f8f8bae077a8783ad45e8d
Shivam Sharma <meshivam81@gmail.com>
2023-09-09 07:03:13 +0530

    jest dom

--- dashboard/components/dashboard/components/cloud-map/DashboardCloudMap.test.tsx
+++ dashboard/components/dashboard/components/cloud-map/DashboardCloudMap.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import DashboardCloudMap, { DashboardCloudMapProps } from './DashboardCloudMap';
 
 const props: DashboardCloudMapProps = {
